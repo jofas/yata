4ad86e87f12b465fe6b9a0041dba343309bd357b
jofas <jonas@fc-web.de>
2020-10-24 19:36:18 +0100

    abstraction over lists

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,8 @@ class _YataState extends State<Yata> {
   List<String> _todos = []; //["A", "B", "C", "D", "E", "F", "G", "H"];
   List<String> _done = [];
 
+  Elements _elements = new Elements();
+
   FocusNode _focus_node;
   int _index = 0;
 
@@ -55,7 +57,7 @@ class _YataState extends State<Yata> {
     setState(() {
       if (_text_controller.text.length > 0) {
         Navigator.pop(context);
-        _todos.insert(0, _text_controller.text);
+        _elements.addTODO(_text_controller.text);
         _text_controller.clear();
       } else {
         _focus_node.requestFocus();
@@ -75,7 +77,7 @@ class _YataState extends State<Yata> {
               autofocus: true,
               focusNode: _focus_node,
               controller: _text_controller,
-              onSubmitted: (String _) {addTODO();},
+              onSubmitted: (_) {addTODO();},
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: "Enter a TODO item",
@@ -88,15 +90,11 @@ class _YataState extends State<Yata> {
                 Navigator.pop(context);
                 _text_controller.clear();
               },
-              child: const Text(
-                "CANCEL",
-              ),
+              child: const Text("CANCEL"),
             ),
             ElevatedButton(
               onPressed: () {addTODO();},
-              child: const Text(
-                "ADD TODO",
-              ),
+              child: const Text("ADD TODO"),
             )
           ],
         );
@@ -104,6 +102,7 @@ class _YataState extends State<Yata> {
     );
   }
 
+  // TODO: abstract this thing into its own class
   generateTODOPage(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
@@ -113,15 +112,13 @@ class _YataState extends State<Yata> {
           style: Theme.of(context).textTheme.headline3,
         ),
         Expanded(
-          flex: 2,
-          child: _todos.length == 0 ? const Center(child: Text(_nothing_todo)) : Scrollbar(
+          child: _elements.todos.length == 0 ? const Center(child: Text(_nothing_todo)) : Scrollbar(
             controller: _scroll_controller,
             isAlwaysShown: true,
-            //       done in its own page (buttomnavigationbar)
             child: ListView.builder(
               padding: EdgeInsets.all(16.0),
               controller: _scroll_controller,
-              itemCount: _todos.length,
+              itemCount: _elements.todos.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
                   child: Padding(
@@ -139,12 +136,11 @@ class _YataState extends State<Yata> {
                         padding: EdgeInsets.all(16.0),
                         child: Row(
                           children: <Widget>[
-                            Expanded(child: Text(_todos[index])),
+                            Expanded(child: Text(_elements.todos[index])),
                             ElevatedButton(
                               onPressed: () {
                                 setState(() {
-                                  _done.insert(0, _todos[index]);
-                                  _todos.removeAt(index);
+                                  _elements.setDone(index);
                                 });
                               },
                               child: const Icon(Icons.check),
@@ -152,7 +148,7 @@ class _YataState extends State<Yata> {
                             TextButton(
                               onPressed: () {
                                 setState(() {
-                                  _todos.removeAt(index);
+                                  _elements.setTODODeleted(index);
                                 });
                               },
                               child: const Icon(Icons.clear),
@@ -181,19 +177,18 @@ class _YataState extends State<Yata> {
         ),
         Expanded(
           flex: 1,
-          child: _done.length == 0 ? const Center(child: Text(_nothing_done)) : Scrollbar(
+          child: _elements.done.length == 0 ? const Center(child: Text(_nothing_done)) : Scrollbar(
             controller: _scroll_controller,
             isAlwaysShown: true,
             child: ListView(
-              children: _done.asMap().map((int key, String val) {
+              children: _elements.done.asMap().map((int key, String val) {
                 return MapEntry(key, Row(
                   children: <Widget>[
                     Expanded(child: Text(val)),
                     TextButton(
                       onPressed: () {
                         setState(() {
-                          _done.removeAt(key);
-                          _todos.insert(0, val);
+                          _elements.unsetDone(key);
                         });
                       },
                       child: const Icon(Icons.restore),
@@ -201,7 +196,7 @@ class _YataState extends State<Yata> {
                     ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          _done.removeAt(key);
+                          _elements.setDoneDeleted(key);
                         });
                       },
                       child: const Icon(Icons.clear),
@@ -233,6 +228,7 @@ class _YataState extends State<Yata> {
         padding: EdgeInsets.all(16.0),
         child: getCurrentPage(context),
       ),
+      // TODO: show this only on first page
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -263,3 +259,121 @@ class _YataState extends State<Yata> {
   }
 }
 
+class Elements {
+  final List<String> todos = [];
+  final List<String> done = [];
+  final List<String> deleted = [];
+
+  addTODO(String value) => todos.insert(0, value);
+
+  setDone(int index) => _move(todos, done, index);
+
+  unsetDone(int index) => _move(done, todos, index);
+
+  setTODODeleted(int index) => _move(todos, deleted, index);
+
+  setDoneDeleted(int index) => _move(done, deleted, index);
+
+  unsetDeleted(int index) => _move(deleted, todos, index);
+
+  deleteCompletely(int index) {
+    deleted.removeAt(index);
+  }
+
+  _move(src, dest, int index) {
+    dest.insert(0, src[index]);
+    src.removeAt(index);
+  }
+}
+
+class YataPage extends StatelessWidget {
+  final _scroll_controller = new ScrollController();
+
+  final String _title;
+  final String _default_text;
+
+  final List<String> _list;
+
+  // TODO: abstract into own class
+  final VoidCallback _main_button_action;
+  final VoidCallback _secondary_button_action;
+
+  final IconData _main_button_icon;
+  final IconData _secondary_button_icon;
+
+  YataPage(this._title, this._default_text, this._list,
+      this._main_button_action, this._secondary_button_action,
+      this._main_button_icon, this._secondary_button_icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "TODO:",
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        Expanded(
+          child: _list.length == 0 ? Center(child: Text(_default_text)) : Scrollbar(
+            controller: _scroll_controller,
+            isAlwaysShown: true,
+            child: ListView.builder(
+              padding: EdgeInsets.all(16.0),
+              controller: _scroll_controller,
+              itemCount: _list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Container(
+                      decoration: ShapeDecoration(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20)
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(child: Text(_list[index])),
+                            // TODO: I need to supply Icon and callback
+                            ElevatedButton(
+                              onPressed: _main_button_action,
+                              /*
+                              () {
+                                setState(() {
+                                  _elements.setDone(index);
+                                });
+                              },
+                              */
+                              child: Icon(_main_button_icon),
+                            ),
+                            TextButton(
+                              onPressed: _secondary_button_action,
+                              /*
+                              () {
+                                setState(() {
+                                  _elements.setTODODeleted(index);
+                                });
+                              },
+                              */
+                              child: Icon(_secondary_button_icon),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
