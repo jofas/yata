320d408f03dbf78a78ef6870c851a35905aca23d
jofas <jonas@fc-web.de>
2020-11-04 16:08:50 +0000

    http api working now

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{Result as ActixResult, get, post, web, App,
-  HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse,
+  HttpServer, Responder};
 use actix_cors::Cors;
 use serde_derive::{Serialize, Deserialize};
 
@@ -9,17 +9,52 @@ extern crate lazy_static;
 use std::sync::Mutex;
 
 #[derive(Serialize, Clone)]
-struct YataData {
+struct Elements {
   todos: Vec<String>,
   done: Vec<String>,
   deleted: Vec<String>
 }
 
-impl YataData {
-  fn new() -> YataData {
-    YataData{todos: vec!["test".to_string()], done: vec!["mest".to_string()],
-      deleted: vec!["rest".to_string()]}
+impl Elements {
+  fn new() -> Self {
+    Self {todos: Vec::new(), done: Vec::new(), deleted: Vec::new()}
   }
+
+  fn add_todo(&mut self, value: String) {
+    self.todos.insert(0, value);
+  }
+
+  fn set_done(&mut self, index: usize) {
+    move_element(&mut self.todos, &mut self.done, index);
+  }
+
+  fn unset_done(&mut self, index: usize) {
+    move_element(&mut self.done, &mut self.todos, index);
+  }
+
+  fn unset_deleted(&mut self, index: usize) {
+    move_element(&mut self.deleted, &mut self.todos, index);
+  }
+
+  fn set_todo_deleted(&mut self, index: usize) {
+    move_element(&mut self.todos, &mut self.deleted, index);
+  }
+
+  fn set_done_deleted(&mut self, index: usize) {
+    move_element(&mut self.done, &mut self.deleted, index);
+  }
+
+  fn delete_completely(&mut self, index: usize) {
+    self.deleted.remove(index);
+  }
+
+  fn delete_all_completely(&mut self) {
+    self.deleted = Vec::new();
+  }
+}
+
+fn move_element(src: &mut Vec<String>, dest: &mut Vec<String>, index: usize) {
+  dest.insert(0, src.remove(index));
 }
 
 #[derive(Deserialize)]
@@ -29,56 +64,84 @@ struct AddTodo {
 
 // data as global variable
 lazy_static! {
-  static ref DATA: Mutex<YataData> = Mutex::new(YataData::new());
+  static ref DATA: Mutex<Elements> = Mutex::new(Elements::new());
 }
 
 #[get("/")]
-async fn all(_req: HttpRequest) -> ActixResult<web::Json<YataData>> {
-  // TODO: to json syntax of HttpResponse
-  Ok(web::Json((*DATA.lock().unwrap()).clone()))
+async fn get_elements() -> impl Responder {
+  HttpResponse::Ok().json((*DATA.lock().unwrap()).clone())
 }
 
-#[post("/todos")]
-async fn todos(todo: web::Json<AddTodo>) -> impl Responder {
-  DATA.lock().unwrap().todos.insert(0, todo.value.clone());
+#[post("/add_todo")]
+async fn add_todo(todo: web::Json<AddTodo>) -> impl Responder {
+  DATA.lock().unwrap().add_todo(todo.value.clone());
+  println!("Adding TODO: {}", todo.value);
   HttpResponse::Ok().finish()
 }
 
-#[get("/done")]
-async fn done() -> impl Responder {
-  HttpResponse::Ok().body("Hello!")
+#[post("/set_done/{index}")]
+async fn set_done(web::Path((index,)): web::Path<(usize,)>) -> impl Responder {
+  DATA.lock().unwrap().set_done(index);
+  println!("Setting TODO to done: {}", index);
+  HttpResponse::Ok().finish()
 }
 
-#[get("/deleted")]
-async fn deleted() -> impl Responder {
-  HttpResponse::Ok().body("Hello!")
+#[post("/unset_done/{index}")]
+async fn unset_done(web::Path((index,)): web::Path<(usize,)>) -> impl Responder {
+  DATA.lock().unwrap().unset_done(index);
+  println!("Unsetting done: {}", index);
+  HttpResponse::Ok().finish()
 }
 
-// TODO:
-//
-// GET / gets the whole deal
-// POST /todos {value} add new item
-// POST /todo_set_done {id}
-// POST /todo_set_deleted {id}
-// POST /done_set_todo {id}
-// POST /done_set_deleted {id}
-// POST /deleted_set_todo {id}
-// POST /delete_completely {id}
-//
-//
-// connect the API to a database
-//
-// websocket API for live changes instead of HTTP API
+#[post("/unset_deleted/{index}")]
+async fn unset_deleted(web::Path((index,)): web::Path<(usize,)>) -> impl Responder {
+  DATA.lock().unwrap().unset_deleted(index);
+  println!("Unsetting deleted: {}", index);
+  HttpResponse::Ok().finish()
+}
+
+#[post("/set_todo_deleted/{index}")]
+async fn set_todo_deleted(web::Path((index,)): web::Path<(usize,)>) -> impl Responder {
+  DATA.lock().unwrap().set_todo_deleted(index);
+  println!("Setting TODO to deleted: {}", index);
+  HttpResponse::Ok().finish()
+}
+
+#[post("/set_done_deleted/{index}")]
+async fn set_done_deleted(web::Path((index,)): web::Path<(usize,)>) -> impl Responder {
+  DATA.lock().unwrap().set_done_deleted(index);
+  println!("Setting done to deleted: {}", index);
+  HttpResponse::Ok().finish()
+}
+
+#[post("/delete_completely/{index}")]
+async fn delete_completely(web::Path((index,)): web::Path<(usize,)>) -> impl Responder {
+  DATA.lock().unwrap().delete_completely(index);
+  println!("deleting completely: {}", index);
+  HttpResponse::Ok().finish()
+}
+
+#[post("/delete_completely")]
+async fn delete_all_completely() -> impl Responder {
+  DATA.lock().unwrap().delete_all_completely();
+  println!("deleting all completely");
+  HttpResponse::Ok().finish()
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   HttpServer::new(|| {
     App::new()
       .wrap(Cors::permissive())
-      .service(all)
-      .service(todos)
-      .service(done)
-      .service(deleted)
+      .service(get_elements)
+      .service(add_todo)
+      .service(set_done)
+      .service(unset_done)
+      .service(unset_deleted)
+      .service(set_todo_deleted)
+      .service(set_done_deleted)
+      .service(delete_completely)
+      .service(delete_all_completely)
   })
   .bind("127.0.0.1:9999")?
   .run()
